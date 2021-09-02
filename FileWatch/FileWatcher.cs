e22ba91292549e464ff79acc23ff75f11eea64d9
narojay <hj200812@126.com>
2021-09-02 22:17:07 +0800

    添加项目文件。

---
+++ FileWatch/FileWatcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FileWatch
+{
+    public class FileWatcher
+    {
+        private readonly FileSystemWatcher _fileSystemWatcher;
+        private int a = 0;
+        public FileWatcher(string path)
+        {
+            _fileSystemWatcher = new FileSystemWatcher(path);
+            _fileSystemWatcher.Created += OnFileSystemWatcherOnCreated;
+            _fileSystemWatcher.Changed += OnFileSystemWatcherOnChanged;
+            _fileSystemWatcher.Deleted += OnFileSystemWatcherOnDeleted;
+            _fileSystemWatcher.Error += OnFileSystemWatcherOnError;
+        }
+
+        private void OnFileSystemWatcherOnError(object sender, ErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnFileSystemWatcherOnDeleted(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnFileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Start()
+        {
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+        public void End()
+        {
+            _fileSystemWatcher.EnableRaisingEvents = false;
+        }
+        private void OnFileSystemWatcherOnCreated(object sender, FileSystemEventArgs e)
+        {
+            FileRead(e.FullPath);
+        }
+
+        private void FileRead(string path)
+        {
+            try
+            {
+                Thread.Sleep(100);
+
+                using (var file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    var bytes = new byte[20480];
+                    file.Read(bytes, 0, bytes.Length);
+                }
+                Console.WriteLine(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ FileWatch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileSystemWatcher = new FileWatcher("D:\\invoice");
+            fileSystemWatcher.Start();
+            Console.ReadLine();
+        }
+    }
+}
